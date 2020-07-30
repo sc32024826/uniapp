172a9ecb887319fc9e98123ee932c210701ccedd
Shencheng <shencheng_1014@aliyun.com>
2020-07-30 14:22:32 +0800

    生产线停止 功能还未完成,页面以及数据完成

--- src/pages/offline/classify.js
+++ src/pages/offline/classify.js
@@ -1,41 +1,97 @@
 // 只区分颜色  不区分尺码
 export const SelectByColor = data => {
-
+	var MO = new Set()
+	var temp = ''
+	data.map((element) => {
+		temp = JSON.stringify({
+			MO: element.MO,
+			StyleNo: element.StyleNo,
+			ColorName: element.ColorName
+		})
+		MO.add(temp)
+	})
+	var title = [...MO].map(e => JSON.parse(e)) // json 字符串数组
+	var sum = 0
+	var ele = {}
+	var res = []
+	for (var i = 0; i < title.length; i++) {
+		sum = 0
+		for (var j = 0; j < data.length; j++) {
+			if (title[i].MO == data[j].MO && title[i].ColorName == data[j].ColorName) {
+				sum += data[j].Qty
+			}
+		}
+		ele = Object.assign(title[i], { 'Qty': sum })
+		res.push(ele)
+	}
+	console.log(res);
+	return res
 }
 
-// 只区尺码 不区分颜色
+// 只区分尺码 不区分颜色
 export const SelectBySize = data => {
-
+	var MO = new Set()
+	var temp = ''
+	data.map((element) => {
+		temp = JSON.stringify({
+			MO: element.MO,
+			StyleNo: element.StyleNo,
+			SizeName: element.SizeName
+		})
+		MO.add(temp)
+	})
+	var title = [...MO].map(e => JSON.parse(e)) // json 字符串数组
+	var sum = 0
+	var ele = {}
+	var res = []
+	console.log(title);
+	console.log(data);
+	for (var i = 0; i < title.length; i++) {
+		sum = 0
+		for (var j = 0; j < data.length; j++) {
+			if (title[i].MO == data[j].MO && title[i].SizeName == data[j].SizeName) {
+				sum += data[j].Qty
+			}
+		}
+		ele = Object.assign(title[i], { 'Qty': sum })
+		res.push(ele)
+	}
+	console.log(res);
+	return res
 }
 
 // 区分颜色  区分尺码
 export const SelectByColorSize = data => {
-
+	return data
 }
 // 不区分颜色和尺码
-export const SelectAll = data =>{
+export const SelectAll = data => {
 	// 去重 求出 所有MO
 	var s = new Set()
+	var temp = ''
 	data.forEach(e => {
-		s.add(e.MO)
+		temp = JSON.stringify({
+			MO: e.MO,
+			StyleNo: e.StyleNo
+		})
+		s.add(temp)
 	})
-	var MOs = [...s]
+	var MOs = [...s].map(e => JSON.parse(e))
 	// 不区分颜色  不区分尺码 SUM求和
 	console.log(MOs);
 	var res = []
+	var ele = {}
 	MOs.forEach(v => {
 		var SUM = 0
 		console.log(v);
 		data.forEach(e => {
-			if (e.MO == v) {
+			if (e.MO == v.MO) {
 				SUM += e.Qty
 			}
 		})
-		res.push({
-			'MO': v,
-			'Qty': SUM,
-		})
+		ele = Object.assign(v, { 'Qty': SUM })
+		res.push(ele)
 	})
+	console.log(res);
 	return res
 }
-
--- src/pages/offline/offline.vue
+++ src/pages/offline/offline.vue
@@ -1,38 +1,42 @@
 <template>
 	<view id="container">
-		<view id="toolbar" class="flex center">
-			<view class="debug dropdown">
-				<picker @change="bindPickerChange" :value="index" :range="array">
-					<view>选择工序: {{array[index]}}</view>
-				</picker>
+		<view id="head">
+			<view id="toolbar" class="flex center">
+				<view class="dropdown">
+					<picker @change="bindPickerChange" :value="index" :range="array">
+						<view>选择工序: {{array[index]}}</view>
+					</picker>
+				</view>
+				<checkbox-group @change="requestType">
+					<label>
+						<checkbox value="color" />区分颜色
+					</label>
+					<label>
+						<checkbox value="size" />区分尺码
+					</label>
+				</checkbox-group>
 			</view>
-			<checkbox-group @change="requestType">
-				<label>
-					<checkbox value="color" />区分颜色
-				</label>
-				<label>
-					<checkbox value="size" />区分尺码
-				</label>
-			</checkbox-group>
+			<view id="title" class="flex row vertical-center">
+				<checkbox-group @change="selectAll">
+					<checkbox value="chooseAll"></checkbox>
+				</checkbox-group>
+				<view class="white" style="width: 400rpx;">生产单-款号-颜色-尺码</view>
+				<view class="white" style="width: 80rpx;">线上</view>
+				<view class="white" style="width: 100rpx;">下线数</view>
+			</view>
 		</view>
-		<view id="title" class="flex row vertical-center">
-			<checkbox-group @change="selectAll">
-				<checkbox value="chooseAll"></checkbox>
-			</checkbox-group>
-			<view class="white" style="width: 400rpx;">生产单-款号-颜色-尺码</view>
-			<view class="white" style="width: 80rpx;">线上</view>
-			<view class="white" style="width: 100rpx;">下线数</view>
-		</view>
+
 		<view id="list" class="uni-list">
 			<checkbox-group @change="checkboxChange">
-				<view class="stripe" v-for="(item,i) in items" :key="i">
+				<view v-if="!show">暂无数据</view>
+				<view class="stripe" v-for="(item,i) in tableData" :key="i">
 					<view class="flex row test vertical-center ">
 						<checkbox :value="item.id" :checked="item.Check" />
 						<view class="mo">
 							<view>单号: {{item.MO}}</view>
 							<view>款号: {{item.StyleNo}}</view>
-							<view>颜色: {{item.ColorName}}</view>
-							<view>尺码: {{item.SizeName}}</view>
+							<view v-if="DoColor">颜色: {{item.ColorName}}</view>
+							<view v-if="DoSize">尺码: {{item.SizeName}}</view>
 						</view>
 						<view class="count">{{item.Qty}}</view>
 						<input placeholder="0" type="number" adjust-position :disabled="!item.Check"></input>
@@ -41,55 +45,61 @@
 			</checkbox-group>
 		</view>
 		<view class="flex row bottom">
-			<!-- <button type="primary" size="mini" @click="offline">下线选中的衣服</button> -->
-			<!-- <button type="primary" size="mini" @click="offlineByUser">按指定数量下线</button> -->
+			<button type="primary" size="mini" @click="offline">下线选中的衣服</button>
+			<button type="primary" size="mini" @click="offlineByUser">按指定数量下线</button>
 		</view>
 	</view>
 </template>
 
 <script>
 	import { RackOffline, getSeqNameList } from '@/api/api.js'
-	import { SelectAll } from './classify.js'
+	import { SelectAll, SelectBySize, SelectByColor, SelectByColorSize } from './classify.js'
 	export default {
 		data() {
 			return {
-				items: [],
+				tableData: [],
 				choose: [], // 选中的衣服衣架
 				array: [],
-				index: -1,
-				SeqList: []
+				index: 0,
+				SeqList: [],
+				DoColor: false, //区分颜色
+				DoSize: false, //区分尺码
+				defalutData: []
 			}
 		},
 		methods: {
 			// 全选 全不选
 			selectAll(e) {
 				// 全选
-				if (this.items.length === 0) return
+				if (this.tableData.length === 0) return
 				if (e.detail.value == 'chooseAll') {
 					let choose = []
-					this.items.forEach(element => {
+					this.tableData.forEach(element => {
 						element = Object.assign(element, { Check: true })
 						choose.push(element.id)
 					})
 					this.choose = choose
 				} else {
-					this.items.forEach(element => {
+					this.tableData.forEach(element => {
 						element = Object.assign(element, { Check: false })
 					})
 					this.choose = []
 				}
 			},
 			// 区分尺码 区分颜色
-			async requestType(e) {
+			requestType(e) {
 				let param = e.detail.value
-				this.items = await this.getDate(param)
+				param.indexOf('size') > -1 ? this.DoSize = true : this.DoSize = false
+				param.indexOf('color') > -1 ? this.DoColor = true : this.DoColor = false
+				this.getDate()
 			},
 			// 勾选列表行
 			checkboxChange(e) {
+				console.log('这里');
 				let chooseList = e.detail.value
 				this.choose = chooseList
 				if (chooseList.length == 0) return
-				this.items.forEach(element => {
+				this.tableData.forEach(element => {
 					if (chooseList.indexOf(element.id) > -1) {
 						element.Check = true
 					} else {
@@ -99,29 +109,58 @@
 			},
 			offline() {
 				console.log('下线id为', this.choose);
+				uni.showModal({
+					content: '下线id为' + this.choose,
+					showCancel: false
+				})
 			},
 			offlineByUser() {
 
 			},
-			async getDate(param) {
-				const { res, err, data } = await tempfunction(param)
-				return data
+			// 重新刷新表数据 
+			getDate() {
+				if (this.defalutData.length > 0) {
+					if (this.DoColor) {
+						if (this.DoSize) {
+							this.tableData = SelectByColorSize(this.defalutData)
+						} else {
+							this.tableData = SelectByColor(this.defalutData)
+						}
+					} else if (this.DoSize) {
+						this.tableData = SelectBySize(this.defalutData)
+					} else {
+						this.tableData = SelectAll(this.defalutData)
+					}
+				} else {
+					uni.showModal({
+						content: '请求错误,请选则工序!',
+						showCancel: false
+					})
+				}
 			},
 			async bindPickerChange(e) {
+				this.index = e.target.value
 				let SeqCode = this.SeqList[e.target.value].value
 				const [err, res] = await RackOffline(SeqCode)
 				if (err) {
-					// console.log(err);
 					uni.showModal({
 						content: err
 					})
 				} else {
-
-					
-					let re = SelectAll(res.data.response)
-					this.items = re
-					console.log(re);
+					this.defalutData = res.data.response
+					if (this.DoColor) {
+						if (this.DoSize) {
+							this.tableData = SelectByColorSize(res.data.response)
+						} else {
+							this.tableData = SelectByColor(res.data.response)
+						}
+					} else if (this.DoSize) {
+						this.tableData = SelectBySize(res.data.response)
+					} else {
+						this.tableData = SelectAll(res.data.response)
+					}
 				}
+
 			}
 		},
 		// 下拉刷新
@@ -129,13 +168,13 @@
 		// 	await this.getDate()
 		// 	uni.stopPullDownRefresh();
 		// },
-		watch: {
-
-		},
 		async created() {
 			const [err, res] = await getSeqNameList()
 			if (err) {
-				console.log(err);
+				uni.showModal({
+					content: err,
+					showCancel: false
+				})
 			} else {
 				console.log(res.data.response);
 				let obj = res.data.response
@@ -143,13 +182,51 @@
 				for (let { label, value } of obj) {
 					this.array.push(label)
 				}
-				console.log(this.array);
+			}
+		},
+		computed: {
+			show() {
+				if (this.tableData) {
+					return this.tableData.length > 0 ? true : false
+				} else {
+					return false
+				}
+
 			}
 		}
 	}
 </script>
 
 <style lang="less" scoped>
+	#container {
+		width: 100%;
+		text-align: center;
+		// border: solid 1rpx red;
+
+		#head {
+			position: fixed;
+			top: 0;
+			width: 100%;
+			background-color: white;
+			z-index: 2;
+
+			#toolbar {
+				margin: 5rpx;
+			}
+		}
+
+		#list {
+			margin-top: 150rpx;
+		}
+
+		.bottom {
+			position: fixed;
+			bottom: 50rpx;
+			width: 100%;
+			z-index: 2;
+		}
+	}
+
 	//
 	.test {
 		// border: solid 1px red;
@@ -181,10 +258,7 @@
 	}
 
 	//
-	#container {
-		width: 100%;
-		text-align: center;
-	}
+
 
 	.flex {
 		display: flex;
@@ -204,9 +278,7 @@
 		align-items: center; // 垂直居中
 	}
 
-	#toolbar {
-		margin: 5rpx;
-	}
+
 
 	#title {
 		height: 80rpx;
@@ -225,13 +297,6 @@
 	.stripe:nth-child(even) {
 		background: #273238;
 		color: white;
-	}
-
-	.bottom {
-		position: absolute;
-		bottom: 0;
-		width: 100%;
-		margin-bottom: 50rpx;
 	}
 
 	.debug {
