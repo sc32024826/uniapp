cc84baa94a3301ced7e95735d52d12a3d05a255f
Shencheng <shencheng_1014@aliyun.com>
2020-08-08 13:49:17 +0800

    设置了 路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,33 @@ import modules from './modules'
 import Vue from 'vue'
 //这里仅示范npm安装方式的引入，其它方式引入请看最上面【安装】部分
 import Router from 'uni-simple-router'
+import store from '@/store'
 
 Vue.use(Router)
 //初始化
 const router = new Router({
 	routes: [...modules] //路由表
 });
-
 //全局路由前置守卫
 router.beforeEach((to, from, next) => {
-	next()
+	uni.showLoading({
+		
+	})
+	if(to.name == 'index'){
+		next()
+	}else{
+		if(store.state.userName){
+			console.log(store.state.userName)
+			next()
+		}else{
+			router.push({name:'index'})
+		}
+	}
+
+	
 })
 // 全局路由后置守卫
 router.afterEach((to, from) => {
-
+	uni.hideLoading()
 })
 export default router;
