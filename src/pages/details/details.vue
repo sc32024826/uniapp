595f500dd5dcaf285bfe1f30dd0c1a25b0d743df
Shencheng <shencheng_1014@aliyun.com>
2020-08-12 09:29:10 +0800

    update:站点信息 样式修改

--- src/pages/details/details.vue
+++ src/pages/details/details.vue
@@ -4,16 +4,21 @@
 			<uni-collapse>
 				<uni-collapse-item title="站内衣架" :open="true" class="collapseitem">
 					<view v-for="(v,i) in stationMsg.data" :key="i" class="flex row width">
-						<view>{{v.RackCode}}</view>
-						<view>{{v.MoNo}}</view>
-						<view>款:{{v.StyleNo}}</view>
-						<view>色:{{v.ColorName}}</view>
-						<view>码:{{v.SizeName}}</view>
-						<view>{{v.Qty}}件</view>
+						<view class="flex row line">
+							<view>款: {{v.StyleNo}}</view>
+							<view style="flex-shrink: 1;">色: {{v.ColorName}}</view>
+							<view style="flex-shrink: 1;">码: {{v.SizeName}}</view>
+						</view>
+						<view class="flex row line">
+							<view>单: {{v.MoNo}}</view>
+							<view>号: {{v.RackCode}}</view>
+							<view>{{v.Qty}}件</view>
+						</view>
+
 					</view>
 				</uni-collapse-item>
 				<uni-collapse-item title="已分配的方案">
-					
+
 				</uni-collapse-item>
 			</uni-collapse>
 		</view>
@@ -47,25 +52,49 @@
 </script>
 
 <style lang="less" scoped>
+	.debug {
+		border: solid 1rpx red;
+	}
+
 	#container {
 		width: 100%;
-		.width view{
-			width: 240rpx;
-			text-align: center;
+
+		.line {
+			width: 100%;
 			height: 50rpx;
-			text-overflow: ellipsis;
-			overflow: hidden;
-			white-space: nowrap;
+
+			view {
+				// border-left: solid 2rpx white;
+				// border-right: solid 2rpx white;
+				padding: 4rpx;
+				margin-top: 4rpx;
+				margin-bottom: 4rpx;
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+			}
 		}
-		.width:nth-child(even){
+
+		// .width view {
+		// 	// width: 240rpx;
+		// 	text-align: center;
+		// 	height: 50rpx;
+		// 	text-overflow: ellipsis;
+		// 	overflow: hidden;
+		// 	white-space: nowrap;
+		// }
+
+		.width:nth-child(even) {
 			background-color: #666666;
 			color: white;
 		}
-		.width:nth-child(odd){
+
+		.width:nth-child(odd) {
 			background-color: #55aaff;
 			color: white;
 		}
-		.width{
+
+		.width {
 			border: solid 1rpx black;
 			margin: 2rpx;
 		}
@@ -74,7 +103,8 @@
 	.flex {
 		display: flex;
 	}
-	.row{
+
+	.row {
 		flex-direction: row;
 		justify-content: space-between;
 		flex-wrap: wrap;
